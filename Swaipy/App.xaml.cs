01c946905364d0b8f90e78f4786fbfbd6cf4f339
Ricardo Santos <ricardo@swaipy.com>
2018-05-23 07:32:29 +0900

    using azure

--- Swaipy/App.xaml.cs
+++ Swaipy/App.xaml.cs
@@ -10,8 +10,8 @@ namespace Swaipy
     public partial class App : Application
     {
         //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
-        public static string AzureBackendUrl = "http://localhost:5000";
-        public static bool UseMockDataStore = true;
+        public static string AzureBackendUrl = "http://swaipyapi.azurewebsites.net";
+        public static bool UseMockDataStore = false;
 
         public App()
         {
--- Swaipy/Services/MockDataStore.cs
+++ Swaipy/Services/MockDataStore.cs
@@ -15,13 +15,10 @@ namespace Swaipy.Services
         {
             items = new List<Item>();
             var mockItems = new List<Item>
-            {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." },
+			{
+				{new Item { Id = Guid.NewGuid().ToString(), Text = "Mock Starbucks", Description = "4 points." }},
+				{new Item { Id = Guid.NewGuid().ToString(), Text = "Mock Thai Jasmin", Description = "2 points." }},
+				{new Item { Id = Guid.NewGuid().ToString(), Text = "Mock Burger King", Description = "1 point." }}
             };
 
             foreach (var item in mockItems)
